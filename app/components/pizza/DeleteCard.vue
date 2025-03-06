04e89e934386356a078bde2eecd430e27a35d53c
Alfred Akintunde AMOS <52054578+alfredamos@users.noreply.github.com>
2025-03-06 16:13:02 +0100

    Refactor delete-card, view-card and delete-card.

--- app/components/pizza/DeleteCard.vue
+++ app/components/pizza/DeleteCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-white p-12 max-width-2xl text-black rounded-2xl shadow-2xl">
+  <div class="bg-white p-12 max-width-2xl text-black rounded-2xl shadow-2xl w-1/2">
+    <span className="flex justify-end items-center">
+    <button type="button" className=" text-rose-900 hover:text-zinc-200 hover:bg-indigo-900 py-1 px-2 text-sm rounded-lg flex justify-center items-center text-muted font-semibold" @click="backToList">Close</button>
+  </span>
   <h2 class="font-semibold border-b-2 text-3xl">
     <span>User Delete Confirmation!</span>
   </h2>
@@ -19,10 +22,11 @@
     <span class="block mr-6 text-wrap font-light">Toppings </span>
     <span class="font-semibold w-full text-end">{{ props?.pizza?.topping }}</span>
   </p>
-  <p class="flex justify-between items-center py-2">
-    <span class="font-light">Description</span>
-    <span class="font-semibold text-wrap">{{ props?.pizza?.description }}</span>
-  </p>
+  <p>
+        <span className="font-light mr-5">Description</span>
+        <span className="text-muted mr-4">{{isShowMore ? props?.pizza.description : props?.pizza.description.substring(0,40) }}</span>
+          <button type="button" className=" text-indigo-900 hover:text-zinc-200 hover:bg-indigo-900 py-1 px-2 text-sm rounded-lg flex justify-center items-center text-muted font-semibold" @click="showMoreText">{{isShowMore ? "Less" : "More"}}</button>
+      </p>
   <h4 class="mb-4 mt-4 border-b-2 border-t-2 font-semibold py-4 text-xl">
     <span> Do you really want to delete this user? </span>
   </h4>
@@ -49,11 +53,16 @@
 <script lang="ts" setup>
 import type { Pizza } from '@prisma/client';
 
+//----> State
+const isShowMore = ref(false);
 
-
+//----> Props
 const props = defineProps<{pizza: Pizza}>();
+
+//----> Emits
 const emit = defineEmits(['onBackToList', 'onDelete']);
 
+//----> Handlers
 const backToList = () =>{
     console.log("Back to list!!!")
     emit('onBackToList');
@@ -61,5 +70,9 @@ const backToList = () =>{
 
 const deletePizza = (pizzaId: string) =>{
     emit('onDelete', pizzaId);
-  }
+}
+
+const showMoreText = () => {
+  isShowMore.value = !isShowMore.value
+}
 </script>
--- app/components/pizza/ViewCard.vue
+++ app/components/pizza/ViewCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-  class="bg-slate-100 max-w-lg shadow-2xl flex flex-col gap-2 rounded-2xl overflow-hidden items-center justify-center"
+  class="bg-slate-100 max-w-lg shadow-2xl flex flex-col gap-2 rounded-2xl overflow-hidden items-center justify-center w-1/2"
 >
   <div class="flex 1 w-full h-full">
     <img
@@ -11,6 +11,9 @@
       class="object-cover w-full h-72"
     />
   </div>
+  <span className="flex justify-end items-center">
+    <button type="button" className=" text-rose-900 hover:text-zinc-200 hover:bg-indigo-900 py-1 px-2 text-sm rounded-lg flex justify-center items-center text-muted font-semibold" @click="backToList">Close</button>
+  </span>
   <div class="flex 1 p-10">
     <div class="flex flex-col">
       <h2 class="card-title">
@@ -28,9 +31,10 @@
         <span class="font-light mr-5">Toppings </span>
         <span class="font-semibold text-wrap">{{ props?.pizza?.topping }}</span>
       </p>
-      <p class="flex justify-between items-center">
-        <span class="font-light">Description</span>
-        <span class="font-semibold">{{ props?.pizza?.description }}</span>
+      <p>
+        <span className="font-light mr-5">Description</span>
+        <span className="text-muted mr-4">{{isShowMore ? props?.pizza.description : props?.pizza.description.substring(0,40) }}</span>
+          <button type="button" className=" text-indigo-900 hover:text-zinc-200 hover:bg-indigo-900 py-1 px-2 text-sm rounded-lg flex justify-center items-center text-muted font-semibold" @click="showMoreText">{{isShowMore ? "Less" : "More"}}</button>
       </p>
       <div class="flex justify-center items-center mt-4 w-full">
         <button
@@ -50,11 +54,21 @@
 <script lang="ts" setup>
 import type { Pizza } from '@prisma/client';
 
-const emit = defineEmits(['onCancel'])
+//----> State
+const isShowMore = ref(false);
+
+//----> Emits
+const emit = defineEmits(['onCancel']);
+
+//----> Props.
 const props = defineProps<{pizza: Pizza}>();
 
 const backToList = () =>{
     emit('onCancel');
-  }
+}
+
+const showMoreText = () => {
+  isShowMore.value = !isShowMore.value;
+}
 </script>
 
